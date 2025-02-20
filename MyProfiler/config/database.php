fbc08d104fb6b2672200da2c13d12c2f48198cec
vanhlion <vietanhnguyen32004@gmail.com>
2025-02-20 17:28:56 +0700

    update

--- MyProfiler/config/database.php
+++ MyProfiler/config/database.php
@@ -47,6 +47,7 @@ return [
         'port' => env('DB_PORT', '20947'),
         'database' => env('DB_DATABASE', 'defaultdb'),
         'username' => env('DB_USERNAME', 'avnadmin'),
+        'password' => env('DB_PASSWORD', ''),
         'unix_socket' => env('DB_SOCKET', ''),
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
